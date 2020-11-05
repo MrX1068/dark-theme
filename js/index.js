dcef2b1c9a22db4f3756ff8391766f776025ef3b
Buvaneshvaran <52979995+MrX1068@users.noreply.github.com>
2020-11-06 04:27:15 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("navigation").style.height = "100%";
+    document.getElementById("menuicon").style.display = "none";
+    
+
+}
+
+function closeNav() {
+    document.getElementById("navigation").style.height = "0%";
+    document.getElementById("menuicon").style.display = "initial";
+
+}
